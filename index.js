08801567bcf377bb1e93b9981c65d4791d79436b
Hazel Jane Guno <gunohazeljane@gmail.com>
2026-03-12 09:37:47 +0700

    Fix server: remove MongoClient, keep Express + Mongoose

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const path = require('path');  // <-- needed for file paths
+const path = require('path');
 
 const app = express();
 
-// ---- Config ----
+// Config
 const PORT = 3000;
 const MONGO_URI = 'mongodb+srv://gunohazeljane:hazel_123@cluster0.4casr.mongodb.net/subscriberDB?retryWrites=true&w=majority';
 const JWT_SECRET = 'mysecret123';
@@ -13,14 +13,14 @@ const JWT_SECRET = 'mysecret123';
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));  // <-- serve files in public folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Connect MongoDB
 mongoose.connect(MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
-// Serve the HTML page
+// Serve HTML page
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
